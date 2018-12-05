aa7b7a9ca47a140187b46c6f76b94d2705e243a9
Charlie Booker III <charliebookeriii@Charlies-MacBook-Pro-9.local>
2018-12-05 02:02:03 -0500

    Shiny app being prepared

---
+++ Houston_Apartments/app.R
@@ -0,0 +1,110 @@
+library(tidyverse)
+library(knitr)
+library(fs)
+library(graphics)
+#this library needed to read in excel
+library(readxl)
+#these libraries will be needed to plot map
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(mapdata)
+library(shiny)
+
+#loading in data
+construction <- read_excel("construction.xlsx", skip = 1)
+
+#This theme will get rid of the gridlines but keep the legend
+ditch_the_axes <- theme(
+  axis.text = element_blank(),
+  axis.line = element_blank(),
+  axis.ticks = element_blank(),
+  panel.border = element_blank(),
+  panel.grid = element_blank(),
+  axis.title = element_blank()
+)
+
+
+
+#chosing the state data map
+states <- map_data("state")
+#selecting texas as the state to use
+texas <- states %>%
+  subset(region == "texas")
+
+#Putting in county lines. First I need all county lines and then I will subset for Texas to get Texas county lines
+counties <- map_data("county")
+tx_county <- counties %>%
+  subset(region == "texas") %>%
+  #going to filter for the Houston counties
+  subset(subregion %in% c("harris", "galveston", "brazoria", "fort bend", "waller", "montgomery", "chambers"))
+
+#looking at texas map
+tx_base <- texas %>%
+  ggplot(mapping = aes(x = long, y = lat, group = group)) + coord_fixed(1.3) + geom_polygon(color = "black", fill = "gray")
+
+#This was before, I used gg2 as my Houston map, so I'm commenting this out
+
+#gg1 <- tx_base + theme_nothing() +
+#putting in county lines
+#geom_polygon(data = tx_county, fill = NA, color = "white") + 
+#geom_polygon(color = "black", fill = NA)
+#this puts the state border on top
+
+
+#plotting the points of new construction on the whole state map
+#gg1 + geom_point(data = construction, aes(x = Longitude, y = Latitude, group = NA), color = "yellow", size = 1)
+
+#trying to get Houston plot
+hou_base <- tx_county %>%
+  ggplot(mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) + geom_polygon(color = "black", fill = "gray")
+
+#repeating steps from earlier but Houston counties instead of whole state of Texas. Trying to get Houston plot continued
+gg2 <- hou_base + ditch_the_axes + 
+  geom_polygon(data = tx_county, fill = NA, color = "white") + geom_polygon(color = "black", fill = NA)
+
+gg2 + geom_point(data = construction, aes(x = Longitude, y = Latitude, group = NA), color = "black", size = 1, alpha = .5) +
+  geom_point(data = construction, aes(x = Longitude, y = Latitude, 
+                                      color = Category, group = NA), size = .8, alpha = .5) + scale_color_manual(values = c("red", "blue", "yellow"))
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Old Faithful Geyser Data"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
